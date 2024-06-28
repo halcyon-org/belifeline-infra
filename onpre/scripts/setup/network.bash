0b6ba6377e4a77eac6aae626bb2f28439a95bd4e
6ouma <102800255+6ouma@users.noreply.github.com>
2024-06-28 18:22:23 +0900

    fix(script): Correct the routing table
    Since the default route must be the virtual nic.

--- onpre/scripts/setup/network.bash
+++ onpre/scripts/setup/network.bash
@@ -3,12 +3,15 @@
 set -euo pipefail
 
 dhclient enp2s0
+if (ip a show enp2s0 | grep -E '172.16.2'); then
+  ip route del default
+  ip route add default via 172.16.200.1 dev vmbr0
+fi
+
 if (ip a show enp2s0 | grep -E '10\.[0-9]+\.[0-9]+\.[0-9]+'); then
   echo '## Connect to the SNCT network'
   read -rp 'SNCT network user name: ' NETWORK_USERNAME
   python3 scripts/setup/snct-login.py "$NETWORK_USERNAME"
-  export ALL_PROXY='http://http-p.srv.cc.suzuka-ct.ac.jp:8080'
-  export HTTP_PROXY="$ALL_PROXY" HTTPS_PROXY="$ALL_PROXY"
   while (ip a show enp2s0 | grep -E '10\.[0-9]+\.[0-9]+\.[0-9]+'); do
     sleep 1
   done
--- onpre/setup.bash
+++ onpre/setup.bash
@@ -4,4 +4,6 @@ set -euo pipefail
 
 ./scripts/setup/proxmox.bash
 ./scripts/setup/network.bash
+export ALL_PROXY='http://http-p.srv.cc.suzuka-ct.ac.jp:8080'
+export HTTP_PROXY="$ALL_PROXY" HTTPS_PROXY="$ALL_PROXY"
 ./scripts/setup/vpn.bash
