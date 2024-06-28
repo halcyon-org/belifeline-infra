c38b3cdd305ead311356d15359c154ed3c38a27e
6ouma <102800255+6ouma@users.noreply.github.com>
2024-06-28 18:01:37 +0900

    fix(script): Connect to the network correctly
    Wait until the network is changed.

--- onpre/scripts/setup/network.bash
+++ onpre/scripts/setup/network.bash
@@ -2,10 +2,14 @@
 
 set -euo pipefail
 
+dhclient enp2s0
 if (ip a show enp2s0 | grep -E '10\.[0-9]+\.[0-9]+\.[0-9]+'); then
   echo '## Connect to the SNCT network'
   read -rp 'SNCT network user name: ' NETWORK_USERNAME
   python3 scripts/setup/snct-login.py "$NETWORK_USERNAME"
   export ALL_PROXY='http://http-p.srv.cc.suzuka-ct.ac.jp:8080'
   export HTTP_PROXY="$ALL_PROXY" HTTPS_PROXY="$ALL_PROXY"
+  while (ip a show enp2s0 | grep -E '10\.[0-9]+\.[0-9]+\.[0-9]+'); do
+    sleep 1
+  done
 fi
--- onpre/scripts/setup/proxmox.bash
+++ onpre/scripts/setup/proxmox.bash
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 echo '## Modify APT repositories'
-rm /etc/apt/source.list.d/{ceph,pve-enterprise}.list
+rm -f /etc/apt/source.list.d/{ceph,pve-enterprise}.list
 echo 'Repositories for non-subscrIption' \
   'deb http://download.proxmox.com/debian/pve ookworm pve-no-subscription' \
   'deb http://download.proxmox.com/debian/ceph-quincy bookworm no-subscription' >>/etc/apt/source.list
