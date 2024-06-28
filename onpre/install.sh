71f04dc250097e6b9f70863d1cb239f58c36e3d2
shiron <shiron4710.dev@gmail.com>
2024-06-28 11:20:37 +0900

    fix: install.sh

--- onpre/install.sh
+++ onpre/install.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 set -e
 
-VPN_DOMAIN=vpn.example.com
+if [ $# -gt 0 ]; then
+  VPN_DOMAIN=$1
+else
+  if [ -z "$VPN_DOMAIN" ]; then
+    read -p "Please enter the VPN domain: " VPN_DOMAIN
+  fi
+fi
 
 mkdir -p /usr/local/bin/
 cp dist/* /usr/local/bin/
