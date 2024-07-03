2eb8fff5d66ffa9406471d4d10ad272bf189c570
6ouma <102800255+6ouma@users.noreply.github.com>
2024-07-03 11:26:14 +0900

    fix(script): Get default route by dhclient
    It already update the default route.

--- onpre/scripts/network.bash
+++ onpre/scripts/network.bash
@@ -13,17 +13,7 @@ EOM
   ifup enp2s0
 fi
 
-dhclient enp2s0
-
-if (ip a show enp2s0 | grep -E '172.16.2'); then
-  cat <<EOM
-
-## Already connected to 172.16.2.n
-EOM
-
-  ip route del default
-  ip route add default via 172.16.200.1 dev vmbr0
-fi
+dhclient
 
 if (ip a show enp2s0 | grep -E '10\.[0-9]+\.[0-9]+\.[0-9]+'); then
   cat <<EOM
