d237b8e5636caf67a852f994ce1f0eee5bfd8d50
6ouma <102800255+6ouma@users.noreply.github.com>
2024-07-03 10:45:31 +0900

    style(script): Merge install.bash into setup.bash
    Make it easier to setup.

--- onpre/scripts/vpn.bash
+++ onpre/scripts/vpn.bash
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
-# set -euo pipefail
+set -euo pipefail
 
-if [[ -z "$VPN_PASSWORD" ]]; then
-  echo 'Password for VPN is not set'
-  exit 1
-fi
-
+: "$VPN_PASSWORD"
 export PATH="/usr/local/bin:$PATH"
+export ALL_PROXY='http://http-p.srv.cc.suzuka-ct.ac.jp:8080'
+export HTTP_PROXY="$ALL_PROXY" HTTPS_PROXY="$ALL_PROXY"
+
+echo '## Copy the VPN commands'
+mkdir -p /usr/local/bin/
+cp dist/* /usr/local/bin/ || :
 
 echo '## Create VPN settings'
 vpncmd /CLIENT localhost /CMD NicCreate VPNNIC
 vpncmd /CLIENT localhost /CMD AccountCreate vpn_connection /SERVER:magic.halcyon.cloud.shiron.dev:443 /USERNAME:pve01 /HUB:VPN /NICNAME:VPNNIC
 vpncmd /CLIENT localhost /CMD AccountPasswordSet vpn_connection /PASSWORD "$VPN_PASSWORD" /TYPE:standard
-
 if [[ -n "$ALL_PROXY" ]]; then
   vpncmd /CLIENT localhost /CMD AccountProxyHttp vpn_connection /SERVER:"$(echo "$ALL_PROXY" | awk -F [:/] '{print $4":"$5}')"
 fi
+
+vpncmd /CLIENT localhost /CMD AccountStartupSet vpn_connection
--- onpre/setup.bash
+++ onpre/setup.bash
@@ -2,8 +2,7 @@
 
 set -euo pipefail
 
-./scripts/setup/proxmox.bash
-./scripts/setup/network.bash
-export ALL_PROXY='http://http-p.srv.cc.suzuka-ct.ac.jp:8080'
-export HTTP_PROXY="$ALL_PROXY" HTTPS_PROXY="$ALL_PROXY"
-./scripts/setup/vpn.bash
+./scripts/proxmox.bash
+./scripts/network.bash
+./scripts/vpn.bash
+./scripts/nic.bash
