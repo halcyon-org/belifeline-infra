03efae842d15beb35e040355afdcdfae6b7e735a
6ouma <102800255+6ouma@users.noreply.github.com>
2024-07-03 14:37:43 +0900

    fix(script): Avoid unbound error of variables
    Use `-v` to check the variables are declared.

--- onpre/scripts/vpn.bash
+++ onpre/scripts/vpn.bash
@@ -32,7 +32,7 @@ vpncmd /CLIENT localhost /CMD NicCreate VPNNIC
 vpncmd /CLIENT localhost /CMD AccountCreate vpn_connection /SERVER:"$VPN_DOMAIN":443 /USERNAME:"$VPN_USERNAME" /HUB:VPN /NICNAME:VPNNIC
 vpncmd /CLIENT localhost /CMD AccountPasswordSet vpn_connection /PASSWORD "$VPN_PASSWORD" /TYPE:standard
 
-if [[ -n "$ALL_PROXY" ]]; then
+if [[ -v ALL_PROXY ]]; then
   vpncmd /CLIENT localhost /CMD AccountProxyHttp vpn_connection /SERVER:"$(echo "$ALL_PROXY" | awk -F [:/] '{print $4":"$5}')"
 fi
 
