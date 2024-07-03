e22d7754b417096fde38bf93da465306150018d8
6ouma <102800255+6ouma@users.noreply.github.com>
2024-07-03 11:28:05 +0900

    chore(script): Add comments to the setup.bash
    Output which file is running.

--- onpre/setup.bash
+++ onpre/setup.bash
@@ -2,7 +2,30 @@
 
 set -euo pipefail
 
+cat <<EOM
+
+
+# proxmox.bash
+EOM
 ./scripts/proxmox.bash
+
+cat <<EOM
+
+
+# network.bash
+EOM
 ./scripts/network.bash
+
+cat <<EOM
+
+
+# vpn.bash
+EOM
 ./scripts/vpn.bash
+
+cat <<EOM
+
+
+# nic.bash
+EOM
 ./scripts/nic.bash
