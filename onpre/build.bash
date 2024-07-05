c02ff683b41836b29c7bb8c8ee5b6cc47553ad07
shiron <shiron4710.dev@gmail.com>
2024-07-05 12:51:04 +0900

    feat: fix build

--- onpre/build.bash
+++ onpre/build.bash
@@ -5,6 +5,9 @@ docker build sevpn -t debian-sevpn
 docker run --detach --rm --name debian-sevpn debian-sevpn
 
 mkdir -p dist
-docker cp debian-sevpn:/usr/local/src/SoftEtherVPN/build/ dist/
+docker cp debian-sevpn:/usr/local/src/SoftEtherVPN/build/vpnclient dist/
+docker cp debian-sevpn:/usr/local/src/SoftEtherVPN/build/vpncmd/vpncmd dist/
+docker cp debian-sevpn:/usr/local/src/SoftEtherVPN/build/hamcore.se2 dist/
+docker cp debian-sevpn:/usr/local/src/SoftEtherVPN/build/*.so dist/
 
 docker stop debian-sevpn
