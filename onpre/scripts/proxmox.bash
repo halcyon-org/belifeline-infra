f7371005f9b0914bfa7fb9e641a13b6a96427a86
6ouma <102800255+6ouma@users.noreply.github.com>
2024-07-12 12:19:28 +0900

    fix(script): Proxmox related file path was wrong
    They're completely uncorrect and should be fixed.

--- onpre/scripts/proxmox.bash
+++ onpre/scripts/proxmox.bash
@@ -7,10 +7,15 @@ cat <<EOM
 ## Modify APT repositories
 EOM
 
-rm -f /etc/apt/source.list.d/{ceph,pve-enterprise}.list
-echo '# Repositories for non-subscrIption' \
-  'deb http://download.proxmox.com/debian/pve ookworm pve-no-subscription' \
-  'deb http://download.proxmox.com/debian/ceph-quincy bookworm no-subscription' >>/etc/apt/source.list
+rm -f /etc/apt/sources.list.d/{ceph,pve-enterprise}.list
+if (! grep pve-no-subscription /etc/apt/sources.list); then
+  cat >>/etc/apt/sources.list <<EOM
+
+# Repositories for non-subscrIption
+deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription
+deb http://download.proxmox.com/debian/ceph-quincy bookworm no-subscription
+EOM
+fi
 
 cat <<EOM
 
