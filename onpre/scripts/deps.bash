719450691c267ecbb279a4a7e68be169caadb80d
shiron <shiron4710.dev@gmail.com>
2024-07-15 15:30:18 +0900

    fix: deps install

--- onpre/scripts/deps.bash
+++ onpre/scripts/deps.bash
@@ -7,4 +7,15 @@ cat <<EOM
 ## Install dependencies
 EOM
 
-dpkg -i ./dist/pkg/*.deb
+packages=("libsodium-dev" "dnsmasq" "frr-pythontools")
+
+for pkg in "${packages[@]}"
+do
+    if ( dpkg -l | grep -q "$pkg" ); then
+        echo "$pkg is not installed. Installing..."
+        dpkg -i ./dist/pkg/*.deb
+        break
+    else
+        echo "$pkg is already installed."
+    fi
+done
